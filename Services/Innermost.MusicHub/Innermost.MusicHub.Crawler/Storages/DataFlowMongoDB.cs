059c02bd68f2ec0460ca488b0228a47625dd0822
HongJieSunnn <457406475@qq.com>
2022-04-11 22:09:24 +0800

    Add DataFlowMongoDB in Innermost.MusicHub.Crawler.
    DataFlowMongoDB inherits DataFlowBase and provider a default constructor to get CrawlerMongoDBContext from DependencyInjection

---
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/Storages/DataFlowMongoDB.cs
@@ -0,0 +1,15 @@
+﻿namespace Innermost.MusicHub.Crawler.Storages
+{
+    internal abstract class DataFlowMongoDB:DataFlowBase
+    {
+        protected readonly CrawlerMongoDBContext _dbContext;
+        public DataFlowMongoDB()
+        {
+            _dbContext=DependencyInjection.ServiceProvider.GetRequiredService<CrawlerMongoDBContext>();
+        }
+
+        public abstract override Task HandleAsync(DataFlowContext context);
+
+        public abstract override Task InitializeAsync();
+    }
+}
