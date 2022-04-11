385b8779ebe80b096f73d570f7dd1653be831ae1
HongJieSunnn <457406475@qq.com>
2022-04-11 22:05:32 +0800

    Add CrawlerMongoDBContext in Innermost.MusicHub.Crawler.
    CrawlerMongoDBContext stores singer,album and musicRecord data crawled in mongodb.

---
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/Storages/CrawlerMongoDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Innermost.MongoDBContext.Configurations;
+using MongoDB.Driver;
+
+namespace Innermost.MusicHub.Crawler.Storages
+{
+    internal class CrawlerMongoDBContext : MongoDBContextBase
+    {
+        public IMongoCollection<SingerListEntity> SingerList { get; set; }
+        public IMongoCollection<AlbumEntity> Albums { get; set; }
+        public IMongoCollection<SingerEntity> Singers { get; set; }
+        public CrawlerMongoDBContext(MongoDBContextConfiguration<CrawlerMongoDBContext> configuration) : base(configuration)
+        {
+
+        }
+    }
+}
