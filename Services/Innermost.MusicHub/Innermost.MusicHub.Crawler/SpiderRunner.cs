0b9d261147519bec563ffd01c3757084675f9e99
HongJieSunnn <457406475@qq.com>
2022-04-11 22:17:00 +0800

    Add SpiderRunner in Innermost.MusicHub.Crawler.
    SpiderRunner is responsible for running spiders and controls the execution sequence of spiders.

---
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/SpiderRunner.cs
@@ -0,0 +1,43 @@
+﻿using DotnetSpider.Scheduler;
+using DotnetSpider.Scheduler.Component;
+using Innermost.MusicHub.Crawler.Spiders;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Innermost.MusicHub.Crawler
+{
+    internal class SpiderRunner
+    {
+        public static AutoResetEvent _singerListFinishEvent = new AutoResetEvent(false);
+
+
+        public static Barrier SingerListBarrier = new Barrier(5, (b) => _singerListFinishEvent.Set());
+        public static async Task RunSingerListSpiderAsync()
+        {
+            var builder = Builder.CreateDefaultBuilder<SingerListSpider>();
+            builder.UseSerilog();
+            builder.UseQueueDistinctBfsScheduler<HashSetDuplicateRemover>();
+
+            await builder.Build().RunAsync();
+        }
+
+        public static async Task RunAlbumSpiderAsync()
+        {
+            //_singerListFinishEvent.WaitOne();
+            var builder = Builder.CreateDefaultBuilder<AlbumSpider>();
+            builder.UseSerilog();
+            builder.UseQueueDistinctBfsScheduler<HashSetDuplicateRemover>();
+
+            await builder.Build().RunAsync();
+        }
+
+        public static async Task RunSingerSpiderAsync()
+        {
+            var builder = Builder.CreateDefaultBuilder<SingerSpider>();
+            builder.UseSerilog();
+            builder.UseQueueDistinctBfsScheduler<HashSetDuplicateRemover>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
