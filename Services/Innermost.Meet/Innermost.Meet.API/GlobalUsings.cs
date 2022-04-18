7886b2a8609eeceb0be7982b666ca5577827d388
HongJieSunnn <457406475@qq.com>
2022-04-18 19:03:28 +0800

    Create project Innermost.Meet.API.

---
+++ Services/Innermost.Meet/Innermost.Meet.API/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using Autofac;
+global using Autofac.Extensions.DependencyInjection;
+global using EventBusCommon;
+global using EventBusCommon.Abstractions;
+global using Innermost.Meet.API.Application.IntegrationEventHandles.IntegrationEvents;
+global using Innermost.Meet.Domain.Repositories;
+global using System.Reflection;
+global using ILoggerExtensions;
+global using Innermost.Meet.API.Queries.Models;
+global using CommonService;
+global using CommonService.IdentityService;
+global using MongoDB.Driver;
+global using MongoDB.Driver.Linq;
+global using Innermost.Meet.Infrastructure;
+global using Innermost.Meet.Domain.AggregatesModels.SharedLifeRecord;
---
+++ Services/Innermost.Meet/Innermost.Meet.API/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Innermost.Meet.API.Infrastructure.AutofacModules;
+
+namespace Innermost.Meet.API
+{
+    internal class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Innemost.TagServer.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule<MediatRModule>();
+        }
+    }
+}
