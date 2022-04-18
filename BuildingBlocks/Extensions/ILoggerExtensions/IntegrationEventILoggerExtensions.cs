d5744f4a4e46d7e2bd774bef32b0014bb27e1c78
HongJieSunnn <457406475@qq.com>
2022-04-18 18:57:02 +0800

    Add ILoggerExtensions.IntegrationEventILoggerExtensions.
    Provide the extension method to LogIntegrationEventHandlerStartHandling.

---
+++ BuildingBlocks/Extensions/ILoggerExtensions/IntegrationEventILoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using EventBusCommon;
+using Microsoft.Extensions.Logging;
+
+namespace ILoggerExtensions
+{
+    public static class IntegrationEventILoggerExtensions
+    {
+        public static void LogIntegrationEventHandlerStartHandling<T>(this ILogger<T> logger,IntegrationEvent @event, string appName)
+        {
+            logger.LogInformation("----- Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})", @event.Id, appName, @event);
+        }
+    }
+}
