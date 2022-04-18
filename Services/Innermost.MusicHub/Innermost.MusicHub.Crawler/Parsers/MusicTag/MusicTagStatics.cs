42a0b16ae7b8e7c913619f802eb66aea47b2a5d9
HongJieSunnn <457406475@qq.com>
2022-04-18 20:21:53 +0800

    Add Spider of MusicTag in Innermost.MusicHub.Crawler

---
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/Parsers/MusicTag/MusicTagStatics.cs
@@ -0,0 +1,7 @@
+﻿namespace Innermost.MusicHub.Crawler.Parsers.MusicTag
+{
+    internal class MusicTagStatics
+    {
+        public static HashSet<string> CategoriesNameSet = new HashSet<string>();
+    }
+}
