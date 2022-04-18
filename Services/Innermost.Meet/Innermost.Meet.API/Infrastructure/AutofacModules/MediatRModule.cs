bf37a751a82e23cc3e9d16a808f8dfe66ae7715d
HongJieSunnn <457406475@qq.com>
2022-04-18 19:04:27 +0800

    Add MediatRModule in Innermost.Meet.API.

---
+++ Services/Innermost.Meet/Innermost.Meet.API/Infrastructure/AutofacModules/MediatRModule.cs
@@ -0,0 +1,15 @@
+﻿using MediatR.Extensions.Autofac.DependencyInjection;
+using TagS.Microservices.Client.AutofacExtensions;
+
+namespace Innermost.Meet.API.Infrastructure.AutofacModules
+{
+    internal class MediatRModule: Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterMediatR(typeof(Program).Assembly);
+
+            builder.RegisterTagSMicroservicesClientTypes();
+        }
+    }
+}
