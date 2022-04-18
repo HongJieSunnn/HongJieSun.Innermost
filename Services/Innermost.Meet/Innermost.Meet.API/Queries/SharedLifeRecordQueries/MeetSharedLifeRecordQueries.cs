1ec5bef81caadfed5c5d8526c660a8103efa5e27
HongJieSunnn <457406475@qq.com>
2022-04-18 19:51:30 +0800

    Add queries and dtos for Innermost.Meet.SharedLifeRecord.

---
+++ Services/Innermost.Meet/Innermost.Meet.API/Queries/SharedLifeRecordQueries/MeetSharedLifeRecordQueries.cs
@@ -0,0 +1,67 @@
+﻿using TagS.Microservices.Client.Models;
+
+namespace Innermost.Meet.API.Queries.SharedLifeRecordQueries
+{
+    public class MeetSharedLifeRecordQueries : IMeetSharedLifeRecordQueries
+    {
+        private readonly MeetMongoDBContext _context;
+        private readonly IIdentityService _identityService;
+        public MeetSharedLifeRecordQueries(MeetMongoDBContext context, IIdentityService identityService)
+        {
+            _context = context;
+            _identityService = identityService;
+        }
+        public async Task<IEnumerable<SharedLifeRecordDTO>> GetRandomSharedLifeRecordsAsync(int limit = 20)
+        {
+            var userId = _identityService.GetUserId();
+
+            var records = await _context.SharedLifeRecords.AsQueryable().Where(l => l.UserId != userId).Sample(limit).ToListAsync();//Method Sample(in MongoDD.Driver.Linq) is to get random limit count records.
+
+            return records.Select(r => new SharedLifeRecordDTO(r));
+        }
+
+        public async Task<IEnumerable<SharedLifeRecordDTO>> GetSharedLifeRecordsByLocationAsync(float longitude, float latitude, int page = 1, int limit = 20, double minDistance = 0, double maxDistance = 5000, string sortBy = "Id")
+        {
+            var userId = _identityService.GetUserId();
+
+            var sortByFilter = Builders<SharedLifeRecord>.Sort.Descending(sortBy);//SortBy id,createTime or likesCount.Only descending sort(recent records or most likescount records).
+
+            var nearFilter = Builders<SharedLifeRecord>.Filter.Ne(l => l.UserId, userId) &
+                                Builders<SharedLifeRecord>.Filter.Ne(l => l.Location, null) &
+                                Builders<SharedLifeRecord>.Filter.Near(l => l.Location!.BaiduPOI, latitude, longitude, maxDistance, minDistance);
+
+            var records = await _context.SharedLifeRecords.Find(nearFilter).Sort(sortByFilter).Skip((page - 1) * limit).Limit(limit).ToListAsync();
+
+            return records.Select(r => new SharedLifeRecordDTO(r));
+        }
+
+        public async Task<IEnumerable<SharedLifeRecordDTO>> GetSharedLifeRecordsByMusicRecordAsync(string musicRecordMid, int page = 1, int limit = 20, string sortBy = "Id")
+        {
+            var userId = _identityService.GetUserId();
+
+            var sortByFilter = Builders<SharedLifeRecord>.Sort.Descending(sortBy);
+
+            var filter = Builders<SharedLifeRecord>.Filter.Ne(l => l.UserId, userId) &
+                            Builders<SharedLifeRecord>.Filter.Ne(l => l.MusicRecord, null) &
+                            Builders<SharedLifeRecord>.Filter.Eq(l => l.MusicRecord!.Id, musicRecordMid);
+
+            var records = await _context.SharedLifeRecords.Find(filter).Sort(sortByFilter).Skip((page - 1) * limit).Limit(limit).ToListAsync();
+
+            return records.Select(r => new SharedLifeRecordDTO(r));
+        }
+
+        public async Task<IEnumerable<SharedLifeRecordDTO>> GetSharedLifeRecordsByTagsAsync(IEnumerable<string> tagIds, int page = 1, int limit = 20, string sortBy = "Id")
+        {
+            var userId = _identityService.GetUserId();
+
+            var sortByFilter = Builders<SharedLifeRecord>.Sort.Descending(sortBy);
+
+            var filter = Builders<SharedLifeRecord>.Filter.Ne(l => l.UserId, userId) &
+                            Builders<SharedLifeRecord>.Filter.ElemMatch(l => l.Tags, Builders<TagSummary>.Filter.AnyIn("TagId", tagIds));
+
+            var records = await _context.SharedLifeRecords.Find(filter).Sort(sortByFilter).Skip((page - 1) * limit).Limit(limit).ToListAsync();
+
+            return records.Select(r => new SharedLifeRecordDTO(r));
+        }
+    }
+}
