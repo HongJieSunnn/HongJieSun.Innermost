53e4c27519ec392d082e64ebffc281f6d11488f0
HongJieSunnn <457406475@qq.com>
2022-04-18 20:20:24 +0800

    Update Spider of Singer in Innermost.MusicHub.Crawler

--- Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/Entities/SingerEntity.cs
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Crawler/Entities/SingerEntity.cs
@@ -7,21 +7,21 @@
         [BsonRepresentation(BsonType.String)]
         public string SingerMid { get; set; }
         public string SingerName { get; set; }
-        public string SingerAlias { get; set; }
-        public string SingerNationality { get; set; }
-        public string SingerBirthplace { get; set; }
-        public string SingerOccupation { get; set; }
-        public string SingerBirthday { get; set; }
-        public string SingerRepresentativeWorks { get; set; }
-        public string SingerRegion { get; set; }
-        public string SingerCoverUrl { get; set; }
+        public string? SingerAlias { get; set; }
+        public string? SingerNationality { get; set; }
+        public string? SingerBirthplace { get; set; }
+        public string? SingerOccupation { get; set; }
+        public string? SingerBirthday { get; set; }
+        public string? SingerRepresentativeWorks { get; set; }
+        public string? SingerRegion { get; set; }
+        public string? SingerCoverUrl { get; set; }
         public SingerEntity()
         {
 
         }
-        public SingerEntity(string mid,int singerId, string singerName, string singerAlias,
-            string singerNationality, string singerBirthplace, string singerOccupation, string singerBirthday, string singerRepresentativeWorks,
-            string singerRegion, string singerCoverUrl)
+        public SingerEntity(string mid, int singerId, string? singerName, string? singerAlias,
+            string? singerNationality, string? singerBirthplace, string? singerOccupation, string? singerBirthday, string? singerRepresentativeWorks,
+            string? singerRegion, string? singerCoverUrl)
         {
             SingerMid = mid;
             SingerId = singerId;
