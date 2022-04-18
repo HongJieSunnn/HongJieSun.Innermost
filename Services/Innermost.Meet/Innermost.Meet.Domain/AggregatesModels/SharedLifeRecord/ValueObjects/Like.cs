13ea3cc71f593b022fb1feee995d505decb49acd
HongJieSunnn <457406475@qq.com>
2022-04-18 19:30:13 +0800

    Add SharedLifeRecord aggregate root in Innermost.Meet.Domain.

---
+++ Services/Innermost.Meet/Innermost.Meet.Domain/AggregatesModels/SharedLifeRecord/ValueObjects/Like.cs
@@ -0,0 +1,24 @@
+﻿namespace Innermost.Meet.Domain.AggregatesModels.SharedLifeRecord.ValueObjects
+{
+    public class Like : ValueObject
+    {
+        public string LikeUserId { get; private set; }
+        public string LikeUserName { get; set; }
+        public string LikeUserAvatarUrl { get; private set; }
+        public DateTime LikeTime { get; private set; }
+        public Like(string likeUserId, string likeUserName, string likeUserAvatarUrl, DateTime likeTime)
+        {
+            LikeUserId = likeUserId;
+            LikeUserName = likeUserName;
+            LikeUserAvatarUrl = likeUserAvatarUrl;
+            LikeTime = likeTime;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return LikeUserId;
+            yield return LikeUserName;
+            yield return LikeUserAvatarUrl;
+            yield return LikeTime;
+        }
+    }
+}
