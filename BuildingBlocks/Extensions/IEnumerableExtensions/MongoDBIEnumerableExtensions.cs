6e406a11191e1e03cd5335e1bf20b8d8eefd2a14
HongJieSunnn <457406475@qq.com>
2022-04-22 20:33:54 +0800

    Add IEnumerableExtensions.MongoDBIEnumerableExtensions to combine mongodb fliter definitions.

---
+++ BuildingBlocks/Extensions/IEnumerableExtensions/MongoDBIEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableExtensions
+{
+    public static class MongoDBIEnumerableExtensions
+    {
+        public static FilterDefinition<T> CombineFilterDefinitions<T>(this IEnumerable<FilterDefinition<T>> filterDefinitions)
+        {
+            return filterDefinitions.Aggregate((filterBefore, filterNext) => filterBefore & filterNext);
+        }
+    }
+}
