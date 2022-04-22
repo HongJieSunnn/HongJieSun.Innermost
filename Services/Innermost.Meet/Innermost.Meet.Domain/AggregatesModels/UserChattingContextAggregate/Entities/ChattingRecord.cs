3bbb9a43ee5ea450a562dba17d59bfb3e120758d
HongJieSunnn <457406475@qq.com>
2022-04-22 21:04:19 +0800

    Add UserChattingContextAggregate in Innermost.Meet.Domain which stores chatting records between confidants.

---
+++ Services/Innermost.Meet/Innermost.Meet.Domain/AggregatesModels/UserChattingContextAggregate/Entities/ChattingRecord.cs
@@ -0,0 +1,28 @@
+﻿using TagS.Microservices.Client.DomainSeedWork;
+using TagS.Microservices.Client.Models;
+
+namespace Innermost.Meet.Domain.AggregatesModels.UserChattingAggregate.Entities
+{
+    /// <summary>
+    /// ChattingRecord tagable entity.
+    /// Tag chatting record seems awesome.
+    /// Id is object id.
+    /// </summary>
+    public class ChattingRecord : TagableEntity<string>
+    {
+        public string SendUserId { get; init; }
+        public string RecordMessage { get; init; }
+        public DateTime CreateTime { get; init; }
+        public ChattingRecord(string sendUserId,string recordMessage,DateTime createTime,List<TagSummary> tagSummaries) : base(tagSummaries)
+        {
+            SendUserId = sendUserId;
+            RecordMessage = recordMessage;
+            CreateTime = createTime;
+        }
+
+        protected override IReferrer ToReferrer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Innermost.Meet/Innermost.Meet.Domain/AggregatesModels/UserChattingContextAggregate/UserChattingContext.cs
@@ -0,0 +1,40 @@
+﻿using Innermost.Meet.Domain.AggregatesModels.UserChattingAggregate.Entities;
+using System.Security.Cryptography;
+using TagS.Microservices.Client.DomainSeedWork;
+using TagS.Microservices.Client.Models;
+
+namespace Innermost.Meet.Domain.AggregatesModels.UserChattingAggregate
+{
+    /// <summary>
+    /// UserChatting AggregateRoot.
+    /// </summary>
+    /// <remarks>
+    /// Actually,add List<ChattingRecord> _chattingRecords in UserSocialContact.Confidant is fine too.But should add to each other.
+    /// </remarks>
+    public class UserChattingContext : Entity<string>, IAggregateRoot
+    {
+        [BsonRequired]
+        [BsonElement("Users")]
+        private readonly string[] _users;
+        public IReadOnlyCollection<string> Users => _users;
+
+        [BsonRequired]
+        [BsonElement("ChattingRecords")]
+
+        private List<ChattingRecord> _chattingRecords;
+        public IReadOnlyCollection<ChattingRecord> ChattingRecords => _chattingRecords;
+
+        public UserChattingContext(string userId1,string userId2,List<ChattingRecord> chattingRecords)
+        {
+            _users=new string[2] {userId1,userId2}.OrderBy(s=>s).ToArray();
+            _chattingRecords=chattingRecords;
+        }
+
+        public UserChattingContext(string id,string[] users, List<ChattingRecord> chattingRecords)
+        {
+            Id=id;
+            _users = users.Length == 2 ? users.OrderBy(s=>s).ToArray() : throw new ArgumentException("UserChattingContext's users must be only two.");
+            _chattingRecords = chattingRecords;
+        }
+    }
+}
