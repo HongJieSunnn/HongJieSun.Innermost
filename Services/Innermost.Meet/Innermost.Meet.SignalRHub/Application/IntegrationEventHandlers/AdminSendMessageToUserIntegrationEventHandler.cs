54adf4e9b2c19ce4d4b04ef1aecf739b585af4d6
HongJieSunnn <457406475@qq.com>
2022-04-23 21:45:07 +0800

    Add AdminSendMessageToUserIntegrationEventHandler in Meet.SignalRHub.
    AdminSendMessageToUserIntegrationEventHandler is to handle the message sent by Admin from other services.

---
+++ Services/Innermost.Meet/Innermost.Meet.SignalRHub/Application/IntegrationEventHandlers/AdminSendMessageToUserIntegrationEventHandler.cs
@@ -0,0 +1,68 @@
+﻿using EventBusCommon.Abstractions;
+
+namespace Innermost.Meet.SignalRHub.Application.IntegrationEventHandlers
+{
+    public class AdminSendMessageToUserIntegrationEventHandler : IIntegrationEventHandler<AdminSendMessageToUserIntegrationEvent>
+    {
+        private readonly IHubContext _hubContext;
+        private readonly IUserChattingContextQueries _userChattingContextQueries;
+        private readonly IChattingRecordRedisService _chattingRecordRedisService;
+        private readonly IdentityUserStatueGrpc.IdentityUserStatueGrpcClient _identityUserStatueGrpcClient;
+        public AdminSendMessageToUserIntegrationEventHandler(
+            IHubContext hubContext,
+            IUserChattingContextQueries userChattingContextQueries,
+            IChattingRecordRedisService chattingRecordRedisService, 
+            IdentityUserStatueGrpc.IdentityUserStatueGrpcClient identityUserStatueGrpcClient)
+        {
+            _hubContext = hubContext;
+            _userChattingContextQueries = userChattingContextQueries;
+            _chattingRecordRedisService = chattingRecordRedisService;
+            _identityUserStatueGrpcClient = identityUserStatueGrpcClient;
+        }
+        public async Task Handle(AdminSendMessageToUserIntegrationEvent @event)
+        {
+            var chattingContextId=await _userChattingContextQueries.GetChattingContextIdOfUsers(GetConnectedUserId(),@event.ToUserId);
+
+            await SendMessageToUser(@event.ToUserId, chattingContextId, @event.Message);
+        }
+
+        private bool IsUserOnline(string toUserId)
+        {
+            return _identityUserStatueGrpcClient.IsUserOnline(new IsUserOnlineUserIdGrpcDTO() { UserId = toUserId }).IsOnline;
+        }
+
+        /// <summary>
+        /// Consistent with ChatHub.SendMessageToUser.
+        /// </summary>
+        /// <param name="toUserId"></param>
+        /// <param name="chattingContextId"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private async Task SendMessageToUser(string toUserId, string chattingContextId, string message)
+        {
+            var sendUserId = GetConnectedUserId();
+            if (!IsUserOnline(toUserId))
+            {
+                await _chattingRecordRedisService.AddChattingRecordAsync(chattingContextId, sendUserId, message, false);
+
+                return;
+            }
+
+            var messageId = ObjectId.GenerateNewId().ToString();
+            var chattingRecordDTO = new ChattingRecordDTO(messageId, sendUserId, message, DateTime.Now, null);
+
+            await _hubContext.Clients.User(toUserId).SendAsync("ChattingMessage", chattingRecordDTO);
+
+            await _chattingRecordRedisService.AddChattingRecordAsync(chattingContextId, chattingRecordDTO, true);
+        }
+
+        /// <summary>
+        /// Get admin Id.
+        /// </summary>
+        /// <returns></returns>
+        private string GetConnectedUserId()
+        {
+            return "13B8D30F-CFF8-20AB-8D40-1A64ADA8D067";
+        }
+    }
+}
---
+++ Services/Innermost.Meet/Innermost.Meet.SignalRHub/Application/IntegrationEventHandlers/IntegrationEvents/AdminSendMessageToUserIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿global using EventBusCommon;
+
+namespace Innermost.Meet.SignalRHub.Application.IntegrationEventHandlers.IntegrationEvents
+{
+    public record AdminSendMessageToUserIntegrationEvent:IntegrationEvent
+    {
+        public string ToUserId { get; init; }
+        public string Message { get; set; }
+        public DateTime CreateTime { get; set; }
+        public AdminSendMessageToUserIntegrationEvent(string toUserId,string message)
+        {
+            ToUserId=toUserId;
+            Message=message;
+            CreateTime=DateTime.Now;
+        }
+    }
+}
