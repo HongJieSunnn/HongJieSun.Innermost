c70fae7bfacba22fb7949e796d15557239ef3adb
HongJieSunnn <457406475@qq.com>
2022-04-25 17:55:10 +0800

    Update CommonIdentityService.Extensions.UserIdentityServiceIServiceCollectionExtensions.
    Add grpc client service injection of IdentityUserGrpc.IdentityUserGrpcClient.

--- BuildingBlocks/CommonService/CommonIdentityService/Extensions/UserIdentityServiceIServiceCollectionExtensions.cs
+++ BuildingBlocks/CommonService/CommonIdentityService/Extensions/UserIdentityServiceIServiceCollectionExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using CommonIdentityService.IdentityService;
-using Grpc.Core;
-using Innermost.Identity.API;
+using Innermost.Identity.API.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Net.Http.Headers;
 
 namespace CommonIdentityService.Extensions
 {
@@ -16,6 +14,7 @@ namespace CommonIdentityService.Extensions
         {
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUserIdentityService, UserIdentityService>();
+            services.AddGrpcClient<IdentityUserGrpc.IdentityUserGrpcClient>();
 
             services
                 .AddGrpcClient<IdentityUserGrpc.IdentityUserGrpcClient>(options =>
@@ -24,27 +23,6 @@ namespace CommonIdentityService.Extensions
                 });
 
             //We call grpc to Identity.API in services' actions and which means that if we can call these grpcs,we have already been verified.
-
-
-                 //configure authentication in grpc
-                //.ConfigureChannel((service, options) =>
-                //{
-                //    var httpContextAccessor = service.GetRequiredService<IHttpContextAccessor>();
-                //    var token= httpContextAccessor.HttpContext.Request.Headers[HeaderNames.Authorization];
-
-                //    //https://docs.microsoft.com/zh-cn/aspnet/core/grpc/authn-and-authz?view=aspnetcore-6.0
-                //    //TODO I don't know if it's useful.
-                //    var credentials = CallCredentials.FromInterceptor((context, metadata) =>
-                //    {
-                //        if (!string.IsNullOrEmpty(token))
-                //        {
-                //            metadata.Add("Authorization", $"Bearer {token}");
-                //        }
-                //        return Task.CompletedTask;
-                //    });
-
-                //    options.Credentials = ChannelCredentials.Create(new SslCredentials(), credentials);
-                //});
 
             return services;
         }
