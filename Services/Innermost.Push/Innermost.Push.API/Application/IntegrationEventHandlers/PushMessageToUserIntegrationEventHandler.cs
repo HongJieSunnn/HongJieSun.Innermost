acdea6e4a97a8fd11b8d445960914ee286648edd
HongJieSunnn <457406475@qq.com>
2022-04-26 20:43:48 +0800

    Add PushMessageToUserIntegrationEvent and handler in Innermost.Push.API.

---
+++ Services/Innermost.Push/Innermost.Push.API/Application/IntegrationEventHandlers/PushMessageToUserIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Innermost.Push.API.Application.IntegrationEventHandlers
+{
+    public class PushMessageToUserIntegrationEventHandler : IIntegrationEventHandler<PushMessageToUserIntegrationEvent>
+    {
+        private readonly IHubContext<PushHub> _hubContext;
+        public PushMessageToUserIntegrationEventHandler(IHubContext<PushHub> hubContext)
+        {
+            _hubContext=hubContext;
+        }
+        public async Task Handle(PushMessageToUserIntegrationEvent @event)
+        {
+            await _hubContext.Clients.User(@event.UserId).SendAsync($"Push{@event.Type}Message",@event.Message);
+        }
+    }
+}
