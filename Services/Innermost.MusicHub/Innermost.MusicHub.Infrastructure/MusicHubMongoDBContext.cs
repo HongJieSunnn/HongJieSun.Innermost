9976c079930c622e8a863decdb60b05d724f3cf4
HongJieSunnn <457406475@qq.com>
2022-04-26 20:43:19 +0800

    Update MusicHubMongoDBContext in Innermost.MusicHub.Infrastructure.
    Add indexes creation.

--- Services/Innermost.MusicHub/Innermost.MusicHub.Infrastructure/MusicHubMongoDBContext.cs
+++ Services/Innermost.MusicHub/Innermost.MusicHub.Infrastructure/MusicHubMongoDBContext.cs
@@ -12,9 +12,60 @@ namespace Innermost.MusicHub.Infrastructure
         public IMongoCollection<Album> Albums { get; set; }
         public IMongoCollection<Singer> Singers { get; set; }
         public IMongoCollection<MusicRecord> MusicRecords { get; set; }
-        public MusicHubMongoDBContext(MongoDBContextConfiguration<MusicHubMongoDBContext> configuration) : base(configuration)
+        public MusicHubMongoDBContext(MongoDBContextConfiguration<MusicHubMongoDBContext> configuration,IMediator mediator) : base(configuration)
         {
-            //TODO create indexes
+            _mediator = mediator;
+            CreateIndexes();
+        }
+
+        void CreateIndexes()
+        {
+            CreateIndexesForAlbums();
+            CreateIndexesForSingers();
+            CreateIndexesForMusicRecords();
+        }
+
+        void CreateIndexesForAlbums()
+        {
+            if (!Albums!.Indexes.List().Any())
+            {
+                var albumNameIndex= Builders<Album>.IndexKeys.Text(t => t.AlbumName);
+                var albumGenreIndex= Builders<Album>.IndexKeys.Ascending(t => t.AlbumGenre);
+
+                var createIndexModels = new[] { albumNameIndex, albumGenreIndex }.Select(al => new CreateIndexModel<Album>(al));
+
+                Albums.Indexes.CreateManyAsync(createIndexModels).GetAwaiter().GetResult();
+            }
+        }
+
+        void CreateIndexesForSingers()
+        {
+            if (!Singers!.Indexes.List().Any())
+            {
+                var singerNameIndex= Builders<Singer>.IndexKeys.Text(s=>s.SingerName);
+                var singerRegionIndex= Builders<Singer>.IndexKeys.Ascending(s=>s.SingerRegion);
+
+                var createIndexModels = new[] { singerNameIndex, singerRegionIndex }.Select(i => new CreateIndexModel<Singer>(i));
+
+                Singers.Indexes.CreateManyAsync(createIndexModels).GetAwaiter().GetResult();
+            }
+        }
+
+        void CreateIndexesForMusicRecords()
+        {
+            if (!MusicRecords!.Indexes.List().Any())
+            {
+                var musicNameIndex = Builders<MusicRecord>.IndexKeys.Text(m=>m.MusicName);
+                var musicLyricIndex = Builders<MusicRecord>.IndexKeys.Text(m => m.Lyric);
+                var musicGenreIndex = Builders<MusicRecord>.IndexKeys.Ascending(m=>m.Genre);
+                var musicLanguageIndex = Builders<MusicRecord>.IndexKeys.Ascending(m=>m.Language);
+                var musicTagIdIndex = Builders<MusicRecord>.IndexKeys.Ascending("Tags.TagId");
+                var musicTagNameIndex = Builders<MusicRecord>.IndexKeys.Ascending("Tags.TagName");
+
+                var createIndexModels = new[] { musicNameIndex, musicLyricIndex, musicGenreIndex, musicLanguageIndex, musicTagIdIndex, musicTagNameIndex }.Select(i => new CreateIndexModel<MusicRecord>(i));
+
+                MusicRecords.Indexes.CreateManyAsync(createIndexModels).GetAwaiter().GetResult();
+            }
         }
 
         public async Task<bool> SaveEntitiesAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : Entity<string>,IAggregateRoot
@@ -48,7 +99,7 @@ namespace Innermost.MusicHub.Infrastructure
         {
             if (!_disposed)
             {
-                base.Client.Cluster.Dispose();
+                ClusterRegistry.Instance.UnregisterAndDisposeCluster(Client.Cluster);
                 _disposed = true;
             }
         }
