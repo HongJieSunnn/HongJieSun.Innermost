0983b2eb28c32372291ebbe192df589016853bd4
HongJieSunnn <457406475@qq.com>
2022-04-28 19:36:45 +0800

    Add IntegrationEventServiceSQL project.
    IntegrationEventServiceSQL is for the microservices which use SQLDatabase and EFCore to store data.
    Whatever the microservice need publish events after transaction commis or just publish without Behavior and transaction.This project can do them both.
    What's more,this project provides extension methods to inject more convenient and IntegrationEventSQLModelEntityTypeConfiguration.
    Remember,to add DbSet<IntegrationEventSQLModel> in the microservice's DbContext.

---
+++ BuildingBlocks/EventBus/IntegrationEventServiceSQL/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using EventBusCommon.Abstractions;
+using IntegrationEventServiceSQL.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IntegrationEventServiceSQL.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Use this method in the service which need send integration events and use sql db.
+        /// </summary>
+        /// <typeparam name="TDbContext"></typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddIntegrationEventServiceSQL<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+        {
+            services.AddSingleton<IIntegrationEventService, CommonIntegrationEventServiceSQL<TDbContext>>(s =>
+            {
+                var context = s.GetRequiredService<TDbContext>();
+                var eventBus = s.GetRequiredService<IAsyncEventBus>();
+                var logger = s.GetRequiredService<ILogger<CommonIntegrationEventServiceSQL<TDbContext>>>();
+
+                return new CommonIntegrationEventServiceSQL<TDbContext>(context,eventBus, logger);
+            });
+
+            return services;
+        }
+    }
+}
