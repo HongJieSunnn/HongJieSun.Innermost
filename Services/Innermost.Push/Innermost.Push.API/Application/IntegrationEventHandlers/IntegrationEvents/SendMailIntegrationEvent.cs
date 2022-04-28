76bba838e3d735b06955e86e04dc602af8778ace
HongJieSunnn <457406475@qq.com>
2022-04-28 20:14:22 +0800

    Add SendMailIntegrationEventHandler in Innermost.Push.API

---
+++ Services/Innermost.Push/Innermost.Push.API/Application/IntegrationEventHandlers/IntegrationEvents/SendMailIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace Innermost.Push.API.Application.IntegrationEventHandlers.IntegrationEvents
+{
+    public record SendMailIntegrationEvent:IntegrationEvent
+    {
+        public string ToEmailAddress { get; init; }
+        public string Subject { get; init; }
+        public string Body { get; init; }
+        public bool IsHtml { get; init; }
+        public SendMailIntegrationEvent(string toEmailAddress,string subject,string body,bool ishtml=false)
+        {
+            ToEmailAddress=toEmailAddress;
+            Subject=subject;
+            Body=body;
+            IsHtml=ishtml;
+        }
+    }
+}
---
+++ Services/Innermost.Push/Innermost.Push.API/Application/IntegrationEventHandlers/SendMailIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Innermost.Push.API.Application.IntegrationEventHandlers
+{
+    public class SendMailIntegrationEventHandler : IIntegrationEventHandler<SendMailIntegrationEvent>
+    {
+        private readonly ISendEmailService _sendEmailService;
+        public SendMailIntegrationEventHandler(ISendEmailService sendEmailService)
+        {
+            _sendEmailService=sendEmailService;
+        }
+        public async Task Handle(SendMailIntegrationEvent @event)
+        {
+            await _sendEmailService.SendEmailAsync(@event.ToEmailAddress, @event.Subject, @event.Body, @event.IsHtml);
+        }
+    }
+}
