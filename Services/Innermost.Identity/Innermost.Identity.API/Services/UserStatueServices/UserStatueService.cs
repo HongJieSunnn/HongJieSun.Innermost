1fcc78f0c32e70b34547580bd14cc21af64cecd3
HongJieSunnn <457406475@qq.com>
2022-04-28 19:54:38 +0800

    Add UserStatueService in Innermost.Identity.API.
    So that GrpcService and Conctroller can operate UserStatue in same methods.

---
+++ Services/Innermost.Identity/Innermost.Identity.API/Services/UserStatueServices/UserStatueService.cs
@@ -0,0 +1,69 @@
+﻿using StackExchange.Redis;
+
+namespace Innermost.Identity.API.Services.UserStatueServices
+{
+    public class UserStatueService:IUserStatueService
+    {
+        private readonly UserStatueRedisContext _redisContext;
+        private readonly UserManager<InnermostUser> _userManager;
+        private const string UserOnlineStatueKey = "user_online_statue";
+        private const string UserStatueKey = "user_statue";
+        public UserStatueService(UserStatueRedisContext redisContext,UserManager<InnermostUser> userManager)
+        {
+            _redisContext=redisContext;
+            _userManager=userManager;
+        }
+
+        public async Task<bool> IsUserOnlinedAsync(string userId)
+        {
+            var onlineStatueRedisValue = await _redisContext.Context().HashGetAsync(UserOnlineStatueKey, userId);
+
+            return (bool)onlineStatueRedisValue;
+        }
+
+        public async Task<IEnumerable<bool>> GetUsersOnlinedStatueAsync(IEnumerable<string> userIds)
+        {
+            var onlineStatueRedisValues = await _redisContext.Context().HashGetAsync(UserOnlineStatueKey, userIds.Select(id => new RedisValue(id)).ToArray());
+
+            return onlineStatueRedisValues.Select(rv=>(bool)rv);
+        }
+
+        public async Task<IEnumerable<string>> GetUsersStatueAsync(IEnumerable<string> userIds)
+        {
+            var onlineStatueRedisValues = await _redisContext.Context().HashGetAsync(UserStatueKey, userIds.Select(id => new RedisValue(id)).ToArray());
+
+            return onlineStatueRedisValues.Select(rv => rv.ToString());
+        }
+
+        public async Task SetUserOnlineStatueAsync(string userId, bool online)
+        {
+            await _redisContext.Context().HashSetAsync(UserOnlineStatueKey, userId, online);
+        }
+
+        public async Task SetUserStatueAsync(string userId, string statue)
+        {
+            await SetUserStatueToMySQLAsync(userId, statue);
+            await _redisContext.Context().HashSetAsync(UserStatueKey, userId, statue);
+        }
+
+        private async Task SetUserStatueToMySQLAsync(string userId, string statue)
+        {
+            var userTask = _userManager.FindByIdAsync(userId);
+
+            var user = await userTask;
+            user.UserStatue = statue;
+
+            await _userManager.UpdateAsync(user);
+        }
+
+        public async Task SetUserStatueFromMySQL(InnermostUser user)
+        {
+            var userStatueInRedis=await _redisContext.Context().HashGetAsync(UserStatueKey, user.Id);
+
+            if (userStatueInRedis==RedisValue.Null||userStatueInRedis!=user.UserStatue)
+            {   
+                await _redisContext.Context().HashSetAsync(UserStatueKey, user.Id, user.UserStatue);
+            }
+        }
+    }
+}
