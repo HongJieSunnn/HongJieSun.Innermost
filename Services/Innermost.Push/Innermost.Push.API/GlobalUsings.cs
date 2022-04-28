18f566ef32ff552096744a11034ea3ae499c1a23
HongJieSunnn <457406475@qq.com>
2022-04-28 20:16:17 +0800

    Update Innermost.Push.API project

--- Services/Innermost.Push/Innermost.Push.API/GlobalUsings.cs
+++ Services/Innermost.Push/Innermost.Push.API/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using Innermost.Push.API.Application.IntegrationEventHandlers.Integration
 global using Microsoft.AspNetCore.SignalR;
 global using Innermost.Push.API.Hubs;
 global using Autofac;
+global using Innermost.Push.API.Infrastructure.Services.EmailServices;
