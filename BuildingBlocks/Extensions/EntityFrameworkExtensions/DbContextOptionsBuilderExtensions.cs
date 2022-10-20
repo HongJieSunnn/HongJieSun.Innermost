feb897b250ef9271525448eccaba2ccbad8af0d7
HongJieSun <457406475@qq.com>
2022-10-20 18:21:26 +0800

    Add project EntityFrameworkExtensions
    Split from CommonService
    Publish to nuget in devops.

---
+++ BuildingBlocks/Extensions/EntityFrameworkExtensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ConfigurationExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        public static DbContextOptions<TDbContext> BuildLocalDbContextOptionsMySQL<TDbContext>(this DbContextOptionsBuilder<TDbContext> dbContextOptionsBuilder, string basePath, Type programType, Version? mysqlVersion = null)
+            where TDbContext : DbContext
+        {
+            var config = new ConfigurationBuilder().BuildLocalConfiguration(basePath);
+
+            dbContextOptionsBuilder.UseMySql(config.GetConnectionString("ConnectMySQL"), new MySqlServerVersion(mysqlVersion ?? new Version(5, 7)), options =>
+            {
+                options.MigrationsAssembly(programType.GetTypeInfo().Assembly.GetName().Name);
+            });
+
+            return dbContextOptionsBuilder.Options;
+        }
+    }
+}
