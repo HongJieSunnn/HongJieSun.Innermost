ff305e603153dcf881b37d5f92b8bf0080b20220
HongJieSun <457406475@qq.com>
2022-10-20 18:17:41 +0800

    Update project IntegrationEventServiceSQL
    Publish to nuget in devops.
    Remove all project references to use nuget packages.

--- BuildingBlocks/EventBus/IntegrationEventServiceSQL/GlobalUsings.cs
+++ BuildingBlocks/EventBus/IntegrationEventServiceSQL/GlobalUsings.cs
@@ -1,7 +1,6 @@
-﻿global using System;
+﻿global using EventBusCommon;
+global using Microsoft.EntityFrameworkCore;
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using System.Text;
 global using System.Threading.Tasks;
-global using EventBusCommon;
-global using Microsoft.EntityFrameworkCore;
