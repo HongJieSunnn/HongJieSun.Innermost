ac6d3138c4ac8918168dcc589706f457efa0fbb6
HongJieSun <457406475@qq.com>
2022-10-20 18:07:28 +0800

    Update EventBusServiceBus
    Publish to nuget devops

--- BuildingBlocks/EventBus/EventBusServiceBus/GlobalUsings.cs
+++ BuildingBlocks/EventBus/EventBusServiceBus/GlobalUsings.cs
@@ -1,10 +1,7 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-global using Newtonsoft.Json;
-global using Microsoft.Extensions.Logging;
+﻿global using Autofac;
 global using Azure.Messaging.ServiceBus;
 global using Azure.Messaging.ServiceBus.Administration;
-global using Autofac;
+global using Microsoft.Extensions.Logging;
+global using Newtonsoft.Json;
+global using System;
+global using System.Threading.Tasks;
