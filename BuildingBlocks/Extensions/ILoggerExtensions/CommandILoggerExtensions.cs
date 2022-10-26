4c5aedd23d0f602f8df854d6fc79b6245331d489
HongJieSun <457406475@qq.com>
2022-10-26 17:12:55 +0800

    Update CommandILoggerExtensions's namespace in ILoggerExtensions

--- BuildingBlocks/Extensions/ILoggerExtensions/CommandILoggerExtensions.cs
+++ BuildingBlocks/Extensions/ILoggerExtensions/CommandILoggerExtensions.cs
@@ -1,12 +1,12 @@
-﻿namespace ILoggerExtensions
+﻿namespace Microsoft.Extensions.Logging
 {
     public static class CommandILoggerExtensions
     {
-        public static void LogSendCommand<T>(this ILogger<T> logger,string requestId,string commandName,string commandIdProperty,string commandId,object command)
+        public static void LogSendCommand<T>(this ILogger<T> logger, string requestId, string commandName, string commandIdProperty, string commandId, object command)
         {
             logger.LogInformation(
                 "----- (RequestId:{RequestId}) Sending command: {CommandName} - {IdProperty}: {CommandId} ({@Command})",
-                    requestId,commandName, commandIdProperty, commandId, command);
+                    requestId, commandName, commandIdProperty, commandId, command);
         }
     }
 }
